938926cc99a84ad4ea5db6f09123b3d648236bb1
sahilthakur7 <sahilrd07@gmail.com>
2021-08-10 15:51:45 +0530

    Working till do you have a phone

--- src/components/Step2.js
+++ src/components/Step2.js
@@ -1,25 +1,90 @@
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-
-const schema = yup.object().shape({
-  name: yup.string().required('It is required'),
-  age: yup.number().required(),
-});
+import MainContainer from './MainContainer';
+import { Typography } from '@material-ui/core';
+import Form from './Form';
+import Input from './Input';
+import { step2Schema } from '../validations';
+import { useData } from '../context/DataContext';
+import { FormControlLabel } from '@material-ui/core';
+import { Checkbox } from '@material-ui/core';
+import { useHistory } from 'react-router';
+import PrimaryButton from './PrimaryButton';
 
 const Step2 = () => {
-  const { register, handleSubmit, errors } = useForm({
+  const { data, setValues } = useData();
+  const history = useHistory();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    control,
+  } = useForm({
     mode: 'onBlur',
-    resolver: yupResolver(schema),
+    resolver: yupResolver(step2Schema),
+    defaultValues: {
+      email: data.email,
+      hasPhone: data.hasPhone,
+      phoneNumber: data.phoneNumber,
+    },
   });
 
+  const hasPhone = watch('hasPhone');
+
+  const onSubmit = (data) => {
+    console.log('data-o---', data);
+    setValues(data);
+    // history.push('/step3');
+  };
+
+  console.log('hasPhone ????', hasPhone);
+
   return (
-    <form onSubmit={handleSubmit((d) => console.log(d))}>
-      <input {...register('name')} />
-      <input type="number" {...register('age')} />
-      <input type="submit" />
-      {errors?.name?.message}
-    </form>
+    <MainContainer>
+      <Typography variant="h5" component="h2">
+        Step 2
+      </Typography>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          type="email"
+          name="email"
+          label="Email Name"
+          error={!!errors?.email}
+          helperText={errors?.email?.message}
+          {...register('email')}
+        />
+        {/* <FormControlLabel
+          control={
+            <Checkbox
+              defaultValue={data.hasPhone}
+              defaultChecked={data.hasPhone}
+              color="primary"
+              name="hasPhone"
+              {...register('hasPhone')}
+            />
+          } */}
+        <Controller
+          name="hasPhone"
+          control={control}
+          defaultValue={data.hasPhone}
+          render={({ field }) => <Checkbox {...field} color="primary" />}
+        />
+        Do you have a phone?
+        {hasPhone && (
+          <Input
+            type="text"
+            name="phoneNumber"
+            label="Phone Number"
+            error={!!errors?.phoneNumber}
+            helperText={errors?.phoneNumber?.message}
+            {...register('phoneNumber')}
+          />
+        )}
+        <PrimaryButton>Next</PrimaryButton>
+      </Form>
+    </MainContainer>
   );
 };
 
--- src/validations/index.js
+++ src/validations/index.js
@@ -10,3 +10,10 @@ export const step1Schema = yup.object().shape({
     .matches(/^([^0-9]*)$/, 'Last name should not contain numeric characters')
     .required('Last name is a required field'),
 });
+
+export const step2Schema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Should have correct email format')
+    .required('First name is a required field'),
+});
